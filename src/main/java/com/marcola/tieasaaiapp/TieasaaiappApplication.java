7129cb56f09a19d1b6c2fef1c3a3537127a295f3
Marcola <marcosguimaraest04@gmail.com>
2023-05-25 12:40:59 -0300

    Projeto iniciado

---
+++ src/main/java/com/marcola/tieasaaiapp/TieasaaiappApplication.java
@@ -0,0 +1,13 @@
+package com.marcola.tieasaaiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieasaaiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TieasaaiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcola/tieasaaiapp/TieasaaiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcola.tieasaaiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TieasaaiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
