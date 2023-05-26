af6515f52d366b238c7108a0076e6312039ee6d9
Marcola <marcosguimaraest04@gmail.com>
2023-05-26 17:04:01 -0300

    Adicionado logs no controller de respostas

--- src/main/java/com/marcola/tieasaaiapp/controllers/ResponseController.java
+++ src/main/java/com/marcola/tieasaaiapp/controllers/ResponseController.java
@@ -32,19 +32,25 @@ public class ResponseController {
                     if(sfOp.isPresent()) {
                         SessionFase sf = sfOp.get();
                         sf.setFase(sf.getFase() + 1);
-                        System.out.println("Resposta certa!!");
+                        System.out.println("Resposta certa - Fase: " + fase
+                                            + " - SESSIONID: "  + session.getId()
+                                            + " - Resposta: " + resposta);
                         sfr.save(sf);
                         return "redirect:/";
                     }
-                    System.out.println("Pegou a fase e resposta certa");
+                    System.out.println("Algo deu errado quando o sistema verificou se a sessão estava presente no banco, alerta!!!");
                 }
                 else{
-                    System.out.println("Pegou a fase e resposta errada");
+                    System.out.println("Resposta errada - Fase: " + fase
+                            + " - SESSIONID: "  + session.getId()
+                            + " - Resposta: " + resposta);
                     return "redirect:/";
                 }
-                System.out.println("Pegou a fase");
+                System.out.println("Algo deu errado quando o usuário tentou enviar a resposta, tentou enviar uma resposta com fase inexistente." +
+                        " Fase solicitada: " + fase);
             }
-        System.out.println("Não pegou a fase");
+                System.out.println("Algo deu errado quando o usuário tentou enviar a resposta, tentou enviar uma resposta com fase inexistente." +
+                        " Fase solicitada: " + fase);
                 return "redirect:/";
         }
 }
